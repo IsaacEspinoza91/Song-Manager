148c5bec5ab62e15da1fe5f9a36cb84fedf4be24
IsaacEspinoza91 <isaac.espinoza.b@usach.cl>
2026-03-06 14:55:54 -0300

    Add correo a link

--- Frontend-vue/src/App.vue
+++ Frontend-vue/src/App.vue
@@ -49,7 +49,7 @@ import Icon from './components/common/Icon.vue';
                 </svg>
               </a> -->
               <!-- Email -->
-              <a href="mailto:isaac.espinoza.barria@gmail.com" title="Email">
+              <a href="mailto:isaac.eb@proton.me" title="Email">
                 <svg viewBox="0 0 24 24" class="social-icon">
                   <path fill="currentColor" d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 8L12 13L4 8V6L12 11L20 6V8Z"/>
                 </svg>
